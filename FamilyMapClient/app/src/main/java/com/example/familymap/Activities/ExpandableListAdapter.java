6800748fb4a61f4851510b613c28b6cfcd0e8824
Matthew Phillips <matthew-p@live.com>
2019-12-11 00:36:22 -0700

    closer

--- FamilyMapClient/app/src/main/java/com/example/familymap/Activities/ExpandableListAdapter.java
+++ FamilyMapClient/app/src/main/java/com/example/familymap/Activities/ExpandableListAdapter.java
@@ -1,54 +1,60 @@
 package com.example.familymap.Activities;
 
 import android.content.Context;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.TextView;
 
 import com.example.familymap.Models.Person_Model;
+import com.example.familymap.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     private Context context;
-    private ArrayList<Person_Model> people;
-    private ArrayList<String> relation;
-    public LayoutInflater inflater;
+    private List<String> data;
+    private HashMap<String, List<String>> dataHash;
 
-    ExpandableListAdapter(Context context){
-
+    ExpandableListAdapter(Context context, List<String> data, HashMap<String, List<String>> dataHash){
+        this.context = context;
+        this.data = data;
+        this.dataHash = dataHash;
     }
 
     @Override
     public int getGroupCount() {
-        return 0;
+        return this.data.size();
     }
 
     @Override
     public int getChildrenCount(int groupPosition) {
-        return 0;
+        return this.dataHash.get(this.data.get(groupPosition)).size();
     }
 
     @Override
     public Object getGroup(int groupPosition) {
-        return null;
+        return this.data.get(groupPosition);
     }
 
     @Override
     public Object getChild(int groupPosition, int childPosition) {
-        return null;
+        return this.dataHash.get(this.data.get(groupPosition)).get(childPosition);
     }
 
     @Override
     public long getGroupId(int groupPosition) {
-        return 0;
+        return groupPosition;
     }
 
     @Override
     public long getChildId(int groupPosition, int childPosition) {
-        return 0;
+        return childPosition;
     }
 
     @Override
@@ -58,7 +64,15 @@ public class ExpandableListAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getGroupView(int groupPosition, boolean isExpanded, View convertView, ViewGroup parent) {
-        return null;
+        String headerTitle = (String) getGroup(groupPosition);
+        if(convertView == null){
+            LayoutInflater inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.)
+        }
+        TextView header = convertView.findViewById(R.id.);
+        header.setTypeface(null, Typeface.BOLD);
+        header.setText(headerTitle);
+        return convertView;
     }
 
     @Override
